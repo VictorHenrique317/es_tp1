ca8e93fc2d66f6c26fe42442a8dd3d0c97476894
VictorGabrielMO <victorgabrielengsis@gmail.com>
2024-10-21 17:02:57 -0300

    back added

---
+++ back/project/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+MYSQL_DATABASE_URL = os.getenv("MYSQL_DATABASE_URL")
+if not MYSQL_DATABASE_URL:
+    raise ValueError("Database URL is missing. Please set the MYSQL_DATABASE_URL environment variable.")
+
+engine = create_engine(MYSQL_DATABASE_URL)
+
+Base = declarative_base()
+from project.models import *
+Base.metadata.create_all(bind=engine)
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
---
+++ back/project/models/__init__.py
@@ -0,0 +1,2 @@
+from .user import User
+from .meeting_analysis import MeetingAnalysis
