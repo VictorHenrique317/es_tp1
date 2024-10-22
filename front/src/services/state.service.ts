39d2611b9af40398f0ed8169263ab91951f962e0
Victor <victor.henrique5800@gmail.com>
2024-10-22 11:44:23 -0300

    Frontend feature completo

--- front/src/services/state.service.ts
+++ front/src/services/state.service.ts
@@ -7,68 +7,83 @@ export enum TaskType{
   SUMMARY = "resumir",
 }
 
-export enum TaskProgress{
-  IN_PROGRESS,
-  DONE,
-}
-
 @Injectable({
   providedIn: 'root'
 })
 export class StateService {
-  private file: File | undefined;
-
+  private _file = new BehaviorSubject<File | undefined>(undefined);
   private _current_task = new BehaviorSubject<TaskType | undefined>(undefined);
-  private _task_progress = new BehaviorSubject<TaskProgress | undefined>(undefined);
+  private _task_in_progress = new BehaviorSubject<boolean>(false);
   private _output = new BehaviorSubject<string | undefined>(undefined);
 
+  public file$ = this._file.asObservable();
   public current_task$ = this._current_task.asObservable();
-  public task_progress$ = this._task_progress.asObservable();
+  public task_in_progress$ = this._task_in_progress.asObservable();
   public output$ = this._output.asObservable();
 
   constructor(private api_service: ApiService) { }
 
-  public async postVideo(file: File): Promise<void> {
-    await this.api_service.postVideo(file);
-    this.file = file;
+  public setFile(file: File): void {
+    this._file.next(file);
+  }
+
+  private async postVideo(): Promise<void> {
+    if (this._file.value){
+      await this.api_service.postVideo(this._file.value);
+    }
+    
     return;
   }
 
-  public async postAudio(file: File): Promise<void> {
-    await this.api_service.postAudio(file);
-    this.file = file;
+  private async postAudio(): Promise<void> {
+    if (this._file.value){
+      await this.api_service.postAudio(this._file.value);
+    }
+
+    return;
+  }
+
+  public async postFile(): Promise<void> {
+    if (!this._file.value){ return ;}
+
+    if (this._file.value.type.startsWith('audio/')) {
+      this.postAudio();
+    }else if (this._file.value.type.startsWith('video/')) {
+      this.postVideo();
+    }
+
     return;
   }
 
   public async fetchTranscription(): Promise<void> {
-    if (this._task_progress.value == TaskProgress.IN_PROGRESS) {return;}
+    if (this._task_in_progress.value) {return;}
 
-    this._task_progress.next(TaskProgress.IN_PROGRESS);
+    this._task_in_progress.next(true);
     this._current_task.next(TaskType.TRANSCRIPTION);
     this.api_service.getTranscription().then((transcription) => {
       this._output.next(transcription);
-      this._task_progress.next(TaskProgress.DONE);
+      this._task_in_progress.next(false);
 
     }).catch((error) => {
       this._current_task.next(undefined);
-      this._task_progress.next(undefined);
+      this._task_in_progress.next(false);
       this._output.next(undefined);
       throw error;
     });
   }
 
   public async fetchSummary(): Promise<void> {
-    if (this._task_progress.value == TaskProgress.IN_PROGRESS) {return;}
+    if (this._task_in_progress.value) {return;}
 
-    this._task_progress.next(TaskProgress.IN_PROGRESS);
+    this._task_in_progress.next(true);
     this._current_task.next(TaskType.SUMMARY);
     this.api_service.getSummary().then((summary) => {
       this._output.next(summary);
-      this._task_progress.next(TaskProgress.DONE);
+      this._task_in_progress.next(false);
 
     }).catch((error) => {
       this._current_task.next(undefined);
-      this._task_progress.next(undefined);
+      this._task_in_progress.next(false);
       this._output.next(undefined);
       throw error;
     });
