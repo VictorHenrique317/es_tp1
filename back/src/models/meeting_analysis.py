28dd0e2c0eaa31eaa3cac846267b16e989c79bff
VictorGabrielMO <victorgabrielengsis@gmail.com>
2024-10-23 23:16:03 -0300

    process endpoints implemented

---
+++ back/src/models/meeting_analysis.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String, DateTime, Integer, func
+from sqlalchemy.orm import Column
+from src.database import Base
+
+class MeetingAnalysis(Base):
+    __tablename__ = "meeting_analysis"
+    
+    id =  Column(Integer, primary_key=True)
+    creation_time = Column(DateTime, default=func.now())
+    
+    media_path = Column(String(1024))
+    trascription_path = Column(String(1024)) # Definir tamanho maximo
+    summary_path = Column(String(1024))
+    
+    
